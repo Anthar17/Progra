42575420754fe68b8dec4a48c2e17a3a69ad21b3
Anthar17 <anthar10_601@hotmail.com>
2022-11-27 16:08:14 -0600

    ComprasProg
    La parte solitaria de esto, me confundi de archivo xd

--- Compra.c
+++ Compra.c
@@ -1,81 +1,6 @@
-/*Compra a proveedores*/
 
 #include <stdio.h>
 
-float ganancia(float n);
-
-int main(int argc, char *argv[]) {
-	float costolibro;
-	int si;
-	float Np;
-<<<<<<< Updated upstream
-	
-=======
-	while (si != 2)
-    {
->>>>>>> Stashed changes
-	printf("-------------------Registro compra proveedores-------------------\n");
-	printf("%cDesea ver la ganancia obtenida?\n", 168);
-	printf("Si=1 o no=2\n");
-	scanf("%d", &si);
-	    if (si==1){
-		printf("Ingresa el costo del libro: ");
-		scanf("%f", &costolibro);
-		
-		printf("Ingresa el n%cmero de productos a comprar: ", 163);
-		scanf("%f", &Np);
-		
-		float ganancia(float n);	
-		float gananciafinal;
-		float precioo;
-<<<<<<< Updated upstream
-		
-		
-		gananciafinal=ganancia(costolibro);
-		printf("la ganancia es: %.2f\n", gananciafinal*Np);
-		precioo=((costolibro-gananciafinal));
-=======
-        float gasto;
-		
-		gananciafinal=ganancia(costolibro);
-		printf("la ganancia es: %.2f\n", gananciafinal*Np);
-		precioo=((costolibro+gananciafinal));
->>>>>>> Stashed changes
-		
-		printf("\n\n\nEs decir: ");
-		printf("Si se compran %.0f libros a proveedores, los libros se vender%cn a %.2f y obtiene la ganancia de %.2f", Np, 160, precioo, gananciafinal*Np);
-		
-		int r;
-		
-		printf("\n%cDesea realizar la compra: ? \n Si=1  No=2\n", 168);
-		scanf("%d", &r);
-		if (r==1){
-<<<<<<< Updated upstream
-		printf("\nAgregando...");
-=======
-		printf("\nAgregando...\n\n");
-        /*gasto=(gasto + costolibro*Np);
-        printf("Gasto: %f", gasto);*/
->>>>>>> Stashed changes
-		
-		}else{
-			return 0;
-			
-		}
-<<<<<<< Updated upstream
-		return 0;	
-=======
-			
->>>>>>> Stashed changes
-	}
-		else{
-			return 0;
-		}
-}
-<<<<<<< Updated upstream
-=======
-}
->>>>>>> Stashed changes
 float ganancia(float n){
 	float ganancianeta, ganancia1;
 	int Pa;
@@ -99,4 +24,52 @@ float ganancia(float n){
 	ganancianeta=(ganancia1-n);
 	return ganancianeta;
 	
+}
+
+int main(){
+
+
+printf("Compra de libro\n");
+	float costolibro;
+	int si;
+	float Np;
+	while (si != 2)
+    {
+	printf("-------------------Registro compra proveedores-------------------\n\n");
+	printf("%cDesea ver la ganancia obtenida?\n", 168);
+	printf("Si=1 o no=2\n");
+	scanf("%d", &si);
+	    if (si==1){
+		printf("Ingresa el costo del libro: ");
+		scanf("%f", &costolibro);
+		
+		printf("Ingresa el n%cmero de productos a comprar: ", 163);
+		scanf("%f", &Np);
+		
+		float ganancia(float n);	
+		float gananciafinal;
+		float precioo;
+        float gasto;
+		
+		gananciafinal=ganancia(costolibro);
+		printf("la ganancia es: %.2f\n", gananciafinal*Np);
+		precioo=((costolibro+gananciafinal));
+		
+		printf("\n\n\nEs decir: ");
+		printf("Si se compran %.0f libros a proveedores, los libros se vender%cn a %.2f y obtiene la ganancia de %.2f", Np, 160, precioo, gananciafinal*Np);
+		
+		int r;
+		
+		printf("\n%cDesea realizar la compra: ? \n Si=1  No=2\n", 168);
+		scanf("%d", &r);
+		if (r==1){
+		printf("\nAgregando...\n\n");
+        
+        /*gasto=(gasto + costolibro*Np);
+        printf("Gasto: %f", gasto);*/
+		
+		}
+		
+}
+}
 }
